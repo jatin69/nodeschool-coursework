1911b6566a2119492c2e4a4404526fd819e3dafb
Jatin Rohilla <jatin.mca17.du@gmail.com>
2018-01-05 01:07:14 +0530

    Props

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -12,13 +12,37 @@ export default class TodoBox extends React.Component{
   }
 }
 
+class TD extends React.Component {
+	render () {
+		return (
+			<td style={{border: "1px solid black"}}>
+			{this.props.children}
+			</td>
+			);
+	}
+}
 
+class Todo extends React.Component {
+  render () {
+  	return (
+  		<tr>
+  		<TD>{this.props.title}</TD>
+  		<TD>{this.props.children}</TD>
+  		</tr>
+  		);
+  }
+}
 
 class TodoList extends React.Component {
   render() {
   	return ( 		
 		    <div className="todoList">
-		      I am a TodoList.
+		       <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+            	</table>
 		    </div>
   		);
   }
